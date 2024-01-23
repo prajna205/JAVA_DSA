6de778ccf0d7c13f2c3d1a5312847c6af7ccf728
“prajna205” <sahooprajna73@gmail.com>
2024-01-23 23:54:40 +0530

    Arrays

---
+++ src/array/FirstAndLastOccurance.java
@@ -0,0 +1,18 @@
+package array;
+
+public class FirstAndLastOccurance {
+    public static void main(String[] args) {
+        int[] arr={0 ,0,1, 1, 2, 2, 2, 2};
+        int target =2;
+        int[] result={-1,-1};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == target){
+                if(result[0] == -1)
+                    result[0] =i;
+                result[1]=i;
+            }
+        }//for
+        for(int x: result)
+            System.out.print(x +" ");
+    }
+}
